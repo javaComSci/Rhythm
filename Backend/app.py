fc5732e671b6747e81c33ce336156a80012aa1c6
Dellamoresteven <dellamoresteven@gmail.com>
2019-02-05 16:13:28 -0500

    MySQL setup

--- Backend/app.py
+++ Backend/app.py
@@ -1,22 +1,34 @@
 ##
- # Notes:
- # 1. This langauge is alright...
- # 2. Make sure to run this on local host to test right now!
- # 3. Were gonna need to pass some type of token so that people
- #    cant run our routes from the browser
- # 4. source Backend/bin/activate to get into the Virtual Env
- # 5. pip install -r requirements.txt to install requirements.txt
- #
- #    Steve The Great IV.
+ # Steps to make this thing run:
+ # 1. export FLASK_APP=app.py => Ran in Backend/ => This will make it so you
+ #    auto connect to the Virtual box/container
+ # 2. pip install -r requirements.txt => This downloads all the dependencies
+ # 3. Go check out the google docs folder and make sure you have Backend/config/mysql.json file
+ # 4. Flask run => This should start up the server... maybe..
+ # Note: I dont know if you need mysql downloaded since it is being run of a AWS instance, but
+ # if you run into problems try to download mySQL.
+ # Test: To make sure its running type localhost:5000/ into your browser!
  ##
 
-# Importing Flask, from flask, Also render_template
-from flask import Flask, render_template
+# Importing Flask dependencies
+from flask import Flask, render_template, json, url_for
+
+# Opens mysql.json file to grab mySQL super secret data
+with open("config/mysql.json") as json_file:
+    json_data = json.load(json_file)
+
+# Importing a MySQL helper
+import pymysql
 
 ##
- # Creates an instance of the exisiting class? I THINK
- # Who really knows???????????????
- # __name__ is the name of the class i beleive
+ # Connecting to mySQL with username/password (in .config file)
+ # Connect using mysql -u root -p (Must have mysql downloaded)
+ ##
+db = pymysql.connect(json_data['server'], json_data['username'], json_data['password'], "Rhythm")
+
+##
+ # Creates an instance of the exisiting class/module
+ # __name__ is the name of the module I beleive
  ##
 app = Flask(__name__)
 
@@ -26,7 +38,7 @@ app = Flask(__name__)
  ##
 @app.route("/")
 def home():
-  return "This language is bullshit!"
+    return "This language is really fun..!"
 
 # Something like this can work if we need to send a html file over
   # return render_template('home.html')
@@ -35,9 +47,9 @@ def home():
  # Create a new route with the @app.route tag
  # (def)ine a function, nothello, that will be mapped to '/PythonSucks' route
  ##
-@app.route("/PythonSucks")
+@app.route("/testRoute")
 def nothello():
-    return "This language is bullshit123!"
+    return ":(((())))"
 
 ##
  # This runs the server with debug=on so we can see outputs in the terminal.
