04a396f1747b25724608e76370f258d4d2932b47
Dellamoresteven <dellamoresteven@gmail.com>
2019-02-06 14:02:01 -0500

    MySQLConnect.py

--- Backend/app.py
+++ Backend/app.py
@@ -13,18 +13,10 @@
 # Importing Flask dependencies
 from flask import Flask, render_template, json, url_for
 
-# Opens mysql.json file to grab mySQL super secret data
-with open("config/mysql.json") as json_file:
-    json_data = json.load(json_file)
-
-# Importing a MySQL helper
-import pymysql
-
-##
- # Connecting to mySQL with username/password (in .config file)
- # Connect using mysql -u root -p (Must have mysql downloaded)
- ##
-db = pymysql.connect(json_data['server'], json_data['username'], json_data['password'], "Rhythm")
+# Grabbing the connection file
+import sys
+sys.path.insert(0, 'MySQL/')
+import MySQLConnect
 
 ##
  # Creates an instance of the exisiting class/module
