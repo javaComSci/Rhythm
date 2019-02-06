9badb449194ee24b2c40b4e83dd810d7a7f6f21a
phaseout <mmomain@gmail.com>
2019-02-05 21:12:47 -0500

    removed deprecated code

--- Frontend/App.js
+++ Frontend/App.js
@@ -4,6 +4,9 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from './HomeScreen';
 import ProfileScreen from './ProfileScreen';
 
+/*
+Create the stack navigator
+*/
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
@@ -14,45 +17,7 @@ const AppNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(AppNavigator);
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={{color: '#f19393', fontWeight:'bold', fontSize: 40}}> Rhythm </Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#ffeceb',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// import React from "react";
-// import { View, Text } from "react-native";
-// import { createStackNavigator, createAppContainer } from "react-navigation";
-
-// class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//         <Text>Home Screen</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const AppNavigator = createStackNavigator({
-//   Home: {
-//     screen: HomeScreen
-//   }
-// });
-
-// export default createAppContainer(AppNavigator);
+/*
+Create the app container
+*/
+export default createAppContainer(AppNavigator); // 
