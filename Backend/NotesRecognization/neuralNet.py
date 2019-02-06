7819eacd3b799cfdc932e166db44d823902608ed
javaComSci <iramanat@purdue.edu>
2019-02-06 17:41:15 -0500

    Neural network structure

--- Backend/NotesRecognization/neuralNet.py
+++ Backend/NotesRecognization/neuralNet.py
@@ -1,5 +1,28 @@
 import tensorflow as tf
+from tensorflow import keras
 import numpy as np
+
+# SYMBOL TABLE
+	# - clef
+	# 	- G
+	# 	- C
+	# 	- F
+	# - note
+	# 	- real note
+	# 		- eighth
+	# 		- half
+	# 		- quarter
+	# 		- whole
+	# 	- rest
+	# 		- eighth rest
+	# 		- quarter rest
+	# 		- whole-half rest
+	# 	- extra
+	# 		- sharp
+	# 		- flat
+	# - time
+	# 	- 4 4
+	# 	- 2 2 
 
 # get the data from where it was stored
 def getData():
@@ -9,11 +32,65 @@ def getData():
 	testingOut = np.load('testingOut.npy')
 
 	return trainingIn, trainingOut, testingIn, testingOut
-	
-def trainNN(trainingIn, trainingOut, testingIn, testingOut):
-	pass
 
-	return trainingIn, trainingOut, testingIn, testingOut
+def trainClefNN(trainingIn, trainingOut, testingIn, testingOut):
+	return
+
+def trainNoteNN(trainingIn, trainingOut, testingIn, testingOut):
+	return
+
+def trainTimeNN(trainingIn, trainingOut, testingIn, testingOut):
+	return
+
+def trainExtraNN(trainingIn, trainingOut, testingIn, testingOut):
+	return
+
+def trainRestNN(trainingIn, trainingOut, testingIn, testingOut):
+	return
+
+def trainGeneralNN(trainingIn, trainingOut, testingIn, testingOut):
+	model = keras.Sequential()
+
+def predictClef(symbol):
+	# which clef
+	return
+
+def predictRest(symbol):
+	return
+
+def predictExtra(symbol):
+	return
+
+def predictRealNote(symbol):
+	return
+
+def predictNote(symbol):
+	# what type of note
+	res = None
+	
+	if res == 'realNote':
+		return predictRealNote(symbol)
+	if res == 'rest':
+		return predictRest(symbol)
+	elif res == 'extra':
+		return predictExtra(symbol)
+
+def predictTime(symbol):
+	# time
+	return
+
+def predict(symbol):
+	res = None
+
+	# general classes - clef, note, or time
+	if res == 'clef':
+		return predictClef(symbol)
+	elif res == 'note':
+		return predictNote(symbol)
+	elif res == 'time':
+		return predictTime(symbol)
+
+
 if __name__ == '__main__':
 	trainingIn, trainingOut, testingIn, testingOut = getData()
-	trainNN(trainingIn, trainingOut, testingIn, testingOut)
+	trainGeneralNN(trainingIn, trainingOut, testingIn, testingOut)
