a6f8037594be33a2f086739b61d511aa4b1af2f8
javaComSci <iramanat@purdue.edu>
2019-02-06 23:14:34 -0500

    Created base neural network

--- Backend/NotesRecognization/textToImageParser.py
+++ Backend/NotesRecognization/textToImageParser.py
@@ -5,8 +5,8 @@ from omrdatasettools.image_generators.HomusImageGenerator import HomusImageGener
 
 HomusImageGenerator.create_images(raw_data_directory="./SymbolsDataset",
                                   destination_directory="./SymbolsDatasetParsed",
-                                  stroke_thicknesses=[3],
-                                  canvas_width=96,
-                                  canvas_height=192,
+                                  stroke_thicknesses=[2],
+                                  canvas_width=50,
+                                  canvas_height=70,
                                   staff_line_spacing=14,
                                   staff_line_vertical_offsets=None)
