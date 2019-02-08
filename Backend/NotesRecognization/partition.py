077d6f1bf55364928cebbc54777681c8940a9969
Richard Hansengit config --global user.name Richard <richie.hansen@gmail.com>
2019-02-08 18:29:55 -0500

    updated partition.py

--- Backend/NotesRecognization/partition.py
+++ Backend/NotesRecognization/partition.py
@@ -1,6 +1,27 @@
 import cv2
 import numpy as np
 
+class sheet_object:
+	def __init__(self,tup):
+		self.pixel_list = []
+		self.pixel_list.append(tup)
+
+		self.R1 = self.R2 = tup[0]
+		self.C1 = self.C2 = tup[1]
+
+	def add_pixel(self,tup):
+		self.pixel_list.append(tup)
+
+		if tup[0] < self.R1:
+			self.R1 = tup[0]
+		if tup[0] > self.R2:
+			self.R2 = tup[0]
+		if tup[1] < self.C1:
+			self.C1 = tup[1]
+		if tup[1] > self.C2:
+			self.C2 = tup[1]
+
+
 
 #image - 2d numpy array
 #	return a list of rows that have runs in them
@@ -72,9 +93,10 @@ def remove_vertical_dividers_and_fill(image, dividers):
 			if col > 0 and image[row][col-1] == 255:
 				image[row][col] = 255
 
+#DEPRECATED
 #image - 2d array
 #	Recursive parent function that locates all objects in an image and returns mask of the objects
-def locate_objects(image):
+def locate_objects_DEP(image):
 	mask = np.zeros(image.shape)
 
 	object_count = 0
@@ -86,13 +108,14 @@ def locate_objects(image):
 
 	return mask
 
+#DEPRECATED
 #image - 2d numpy array
 #mask - 2d numpy array
 #row - row index
 #col - column index
 #object_count - current object number
 #	Helper function which recursively locates all new objects
-def locate_object_recur(image,mask,row,col,object_count):
+def locate_object_recur_DEP(image,mask,row,col,object_count):
 
 	if image[row][col] == 255 or mask[row][col] != 0:
 			return
@@ -111,33 +134,83 @@ def locate_object_recur(image,mask,row,col,object_count):
 	if col-1 >= 0:
 		locate_object_recur(image,mask,row,col-1,object_count)
 
-im_gray = cv2.imread("DATA/test3.jpg", cv2.IMREAD_GRAYSCALE)
-# thresh = 195
-# im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
-(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+def locate_objects(image):
+	mask = np.zeros(image.shape)
+	sheet_object_list = []
+	object_count = 1
+
+	for row in range(image.shape[0]):
+		for col in range(image.shape[1]):
+			if image[row][col] == 0:
+				ob_num = scan_pixel(image,mask,row,col)
+
+				if ob_num == -1:
+					mask[row][col] = object_count
+
+					tup = (row,col)
+					new_obj = sheet_object(tup)
+					sheet_object_list.append(new_obj)
+
+					object_count += 1
+				else:
+					mask[row][col] = ob_num
+					tup = (row,col)
+					sheet_object_list[int(ob_num-1)].add_pixel(tup)
+
+	return mask, sheet_object_list
+
+def scan_pixel(image,mask,row,col):
+	startRow = max(row-3,0)
+	startCol = max(col-3,0)
+	endCol = min(image.shape[1], col+5)
+
+	for i in range(startRow, row+1):
+		for j in range(startCol, endCol):
+			if (i != row or j != col) and mask[i][j] != 0:
+				return mask[i][j]
+
+	return -1
+
+def print_objects(SOL):
+	idd = 0
+	for ob in SOL:
+		if len(ob.pixel_list) >= 35:
+			new_img = np.zeros((ob.R2-ob.R1+1,ob.C2-ob.C1+1))
+			for tup in ob.pixel_list:
+				new_img[tup[0] - ob.R1][tup[1] - ob.C1] = 255
+
+			cv2.imwrite("test/oob_{}.jpg".format(idd), new_img)
+			idd += 1
 
 
-#cv2.imshow('image',im_bw)
-#cv2.waitKey(5000)
-#resized_image = cv2.resize(im_bw, (1500, 900))
-# while im_bw.shape[0] > 750 or im_bw.shape[1] > 750:
-# 	im_bw = cv2.resize(im_bw, (0,0), fx=0.50, fy=0.50) 
 
-print(im_bw.shape)
+if __name__ == "__main__":
+	im_gray = cv2.imread("DATA/test4.jpg", cv2.IMREAD_GRAYSCALE)
+	# thresh = 120
+	# im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+	(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	print "Completed 'image load'"
 
+	#cv2.imshow('image',im_bw)
+	#cv2.waitKey(5000)
+	#resized_image = cv2.resize(im_bw, (1500, 900))
+	# while im_bw.shape[0] > 750 or im_bw.shape[1] > 750:
+	# 	im_bw = cv2.resize(im_bw, (0,0), fx=0.50, fy=0.50) 
 
-runs = locate_run_blocks(im_bw)
-remove_runs_and_fill(im_bw, runs)
+	runs = locate_run_blocks(im_bw)
+	remove_runs_and_fill(im_bw, runs)
+	print "Completed 'run' segmenting"
 
-dividers = locate_vertical_dividers(im_bw)
-remove_vertical_dividers_and_fill(im_bw, dividers)
+	dividers = locate_vertical_dividers(im_bw)
+	remove_vertical_dividers_and_fill(im_bw, dividers)
+	print "Completed 'divider' segmenting"
 
-print runs
+	mask, SOL = locate_objects(im_bw)
+	print "Completed 'object location'"
 
-resized_image = cv2.resize(im_bw, (700, 700))
+	print_objects(SOL)
+	print "Completed 'print objects'"
 
-mask = locate_objects(resized_image)
-
-print mask[mask != 0]
-cv2.imshow('image',mask)
-cv2.waitKey(10000)
+	cv2.imshow('image',mask)
+	cv2.waitKey(10000)
