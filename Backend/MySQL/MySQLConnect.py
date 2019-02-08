54071b888955e0a2bc7ec98a5b43f9ea8080914c
Dellamoresteven <dellamoresteven@gmail.com>
2019-02-08 16:44:26 -0500

    Routes

--- Backend/MySQL/MySQLConnect.py
+++ Backend/MySQL/MySQLConnect.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, json, url_for
 import pymysql
 
 # Opens mysql.json file to grab mySQL super secret data
-with open("../config/mysql.json") as json_file:
+with open("config/mysql.json") as json_file:
     json_data = json.load(json_file)
 
 ##
@@ -14,5 +14,13 @@ with open("../config/mysql.json") as json_file:
  # Connect using mysql -u root -p (Must have mysql downloaded)
  ##
 db = pymysql.connect(json_data['server'], json_data['username'], json_data['password'], "Rhythm")
+
 def update():
     print('yesdfea');
+
+
+def insert():
+    print('yesdfea');
+
+
+def delete():
