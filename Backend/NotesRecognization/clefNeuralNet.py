d9cdbec01591271679a382ca48b4a907d3150eff
javaComSci <iramanat@purdue.edu>
2019-02-19 18:17:04 -0500

    String names for the ints

--- Backend/NotesRecognization/clefNeuralNet.py
+++ Backend/NotesRecognization/clefNeuralNet.py
@@ -6,15 +6,22 @@ from keras.models import load_model
 
 model = load_model("clef_model.h5")
 
+translations = np.load('translations.npy')
+translations = translations.item()
+print("TRANLSATIONS", translations)
+
 def predictClef(testingInput):
 	# which clef
-
 	predictions = model.predict(testingInput)
 
 	# actual value of the predictions
-	overallPredictions = -np.ones((testingInput.shape[0],1))
+
+	# overallPredictions = -np.ones((testingInput.shape[0],1))
+	overallPredictions = []
 
 	for i in range(predictions.shape[0]):
-		overallPredictions[i] = np.argmax(predictions[i])
+		currentPrediction = np.argmax(predictions[i])
+		# overallPredictions[i] = translations[currentPrediction]
+		overallPredictions.append(currentPrediction)
 
 	return overallPredictions
