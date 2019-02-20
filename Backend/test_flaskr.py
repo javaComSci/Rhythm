7d5172b12718c9be437fa86629ae1e1cf959d151
javaComSci <iramanat@purdue.edu>
2019-02-20 15:20:47 -0500

    Testing

--- Backend/test_flaskr.py
+++ Backend/test_flaskr.py
@@ -1,21 +1,41 @@
-import os
-import tempfile
+# import os
+# import tempfile
 
-import pytest
+# import pytest
 
-from flaskr import flaskr
+# from flaskr import flaskr
 
 
-@pytest.fixture
-def client():
-    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    flaskr.app.config['TESTING'] = True
-    client = flaskr.app.test_client()
+# @pytest.fixture
+# def client():
+#     db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+#     flaskr.app.config['TESTING'] = True
+#     client = flaskr.app.test_client()
 
-    with flaskr.app.app_context():
-        flaskr.init_db()
+#     with flaskr.app.app_context():
+#         flaskr.init_db()
 
-    yield client
+#     yield client
 
-    os.close(db_fd)
-    os.unlink(flaskr.app.config['DATABASE'])
+#     os.close(db_fd)
+#     os.unlink(flaskr.app.config['DATABASE'])
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/register")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "POST" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
