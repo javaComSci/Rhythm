558c70160452628b5a9e266169af31ac1178c669
javaComSci <iramanat@purdue.edu>
2019-02-20 16:26:21 -0500

    Predictions

--- Backend/NotesRecognization/conversion.py
+++ Backend/NotesRecognization/conversion.py
@@ -4,11 +4,14 @@ import numpy as np
 import cv2
 
 if __name__ == "__main__":
-	file_path = "ExamplePredictions/DATA/test1.jpg"
+	file_path = "ExamplePredictions/DATA/t10.jpg"
 
 	mask, SOL = partition.full_partition(file_path)
 
-	ob_counter = 1
+	ob_counter = 0
+
+	partition.print_objects(mask, SOL, "ExamplePredictions/predictions/")
+
 
 	for ob in SOL:
 		n_arr = partition.SO_to_array(ob)
@@ -22,8 +25,9 @@ if __name__ == "__main__":
 
 		print("DIMS", flat_arr.shape)
 
-		ob_prediction = neuralNet.predict(flat_arr)
+		ob_prediction, im = neuralNet.predict(flat_arr)
+		im = im.reshape((70,50)) * 255
 
-		cv2.imwrite("ExamplePredictions/predictions/ob#{}_label:{}.jpg".format(ob_counter, ob_prediction[0]), n_arr)
+		cv2.imwrite("ExamplePredictions/predictions/ob#{}_label:{}.jpg".format(ob_counter, ob_prediction[0]), im)
 		ob_counter += 1
 	print("Number of objects", ob_counter)
