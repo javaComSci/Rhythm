44c85c3f5e72bfc437f7a838f368358d3cda1249
Dellamoresteven <dellamoresteven@gmail.com>
2019-02-22 17:02:12 -0500

    EditScreen

---
+++ Frontend/components/EditMusicScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput, FlatList, TouchableOpacity, Button, ScrollView, StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
+import { Header } from 'react-navigation';
+import { Button } from 'react-native-elements';
+
+var styles = require('../style');
+
+class EditMusic{
+  constructor(){
+
+  }
+}
