7ccc38956a0f6889c0f070092eab32f98f8eb26a
Richard Hansengit config --global user.name Richard <richie.hansen@gmail.com>
2019-03-01 19:20:26 -0500

    testing with python to json

--- Backend/NotesRecognization/conversion.py
+++ Backend/NotesRecognization/conversion.py
@@ -1,33 +1,44 @@
 import partition
-import neuralNet
+#import neuralNet
 import numpy as np
 import cv2
+import json
+
+class JCpenny:
+	def __init__(self,note,length,pitch):
+		self.note = 1
+		self.length = 1
+		self.pitch = pitch
 
 if __name__ == "__main__":
-	file_path = "ExamplePredictions/DATA/t10.jpg"
+	file_path = "ExamplePredictions/DATA/test3.jpg"
 
-	mask, SOL = partition.full_partition(file_path)
+	mask, SOL, staff_lines = partition.full_partition(file_path)
 
 	ob_counter = 0
 
 	partition.print_objects(mask, SOL, "ExamplePredictions/predictions/")
 
+	dicty = {"clef":1, "notes":[]}
 
-	for ob in SOL:
-		n_arr = partition.SO_to_array(ob)
+	with open('data.json', 'w') as outfile:
+		for ob in SOL:
+			obby = JCpenny(1,1,ob.run)
+			dicty["notes"].append(obby.__dict__)
+		json.dump(dicty, outfile)
 
-		print(n_arr.shape)
-		# resize_arr = cv2.resize(n_arr, (70, 50), interpolation=cv2.INTER_AREA)
-		# print(resize_arr.shape, "RESIDED")
+	# for ob in SOL:
+	# 	n_arr = partition.SO_to_array(ob)
 
-		# flat_arr = resize_arr.flatten().reshape((1,3500))
-		flat_arr = n_arr.flatten().reshape((1,3500))
+	# 	print(n_arr.shape)
 
-		print("DIMS", flat_arr.shape)
+	# 	flat_arr = n_arr.flatten().reshape((1,3500))
 
-		ob_prediction, im = neuralNet.predict(flat_arr)
-		im = im.reshape((70,50)) * 255
+	# 	print("DIMS", flat_arr.shape)
 
-		cv2.imwrite("ExamplePredictions/predictions/ob#{}_label:{}.jpg".format(ob_counter, ob_prediction[0]), im)
-		ob_counter += 1
-	print("Number of objects", ob_counter)
+	# 	ob_prediction, im = neuralNet.predict(flat_arr)
+	# 	im = im.reshape((70,50)) * 255
+
+	# 	cv2.imwrite("ExamplePredictions/predictions/ob#{}_label:{}.jpg".format(ob_counter, ob_prediction[0]), im)
+	# 	ob_counter += 1
+	# print("Number of objects", ob_counter)
