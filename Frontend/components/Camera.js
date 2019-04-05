a9547b0255556c240aa8d386bb28b43a7cf70d57
javaComSci <iramanat@purdue.edu>
2019-04-05 14:11:50 -0400

    Camera permissions

--- Frontend/components/Camera.js
+++ Frontend/components/Camera.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, View, TouchableOpacity, Dimensions, Image } from 'react-native';
 import { Camera, Permissions } from 'expo';
-  // import { Header } from 'react-navigation';
-  import { connect } from 'react-redux';
-  import { Button, Header } from 'react-native-elements';
-  import { Col, Row, Grid } from "react-native-easy-grid";
-  import Icon from 'react-native-vector-icons/AntDesign'
-  import PinchZoomView from 'react-native-pinch-zoom-view';
+// import { Header } from 'react-navigation';
+import { connect } from 'react-redux';
+import { Button, Header } from 'react-native-elements';
+import { Col, Row, Grid } from "react-native-easy-grid";
+import Icon from 'react-native-vector-icons/AntDesign'
+import PinchZoomView from 'react-native-pinch-zoom-view';
 
 
   import { Svg } from 'expo';
@@ -32,6 +32,15 @@ export default class CameraExample extends React.Component {
        const options = { quality: 1, base64: true, fixOrientation: true, 
        exif: true};
        await this.camera.takePictureAsync(options).then(photo => {
+        
+
+        // Upload the image using the fetch and FormData APIs
+        // let formData = new FormData();
+        // Assume "photo" is the name of the form field the server expects
+        // formData.append('photo', { uri: photo, name: 'PHOTO.jpg', type: 'image' });
+        // btoa(formData);
+        // let pizzaMan = atob(formData);
+        // console.log(pizzaMan);
           photo.exif.Orientation = 1;            
            console.log(photo);  
            console.log("I TOOOK A PHOTO!!!")          
@@ -55,16 +64,6 @@ export default class CameraExample extends React.Component {
                 backgroundColor: 'transparent',
                 flexDirection: 'row',
               }}>
-              <TouchableOpacity style={{
-                  position: "absolute",
-                  top: '80%',
-                  right: '44%',
-                  alignItems: 'center',
-                }}
-                onPress={this.snapPhoto.bind(this)}>
-                <Image style={{width: 50, height: 50}} source={require('../assets/capture.png')}          
-                />
-              </TouchableOpacity>
               <Svg height="100%"  width="100%">
                 <Rect x={SCREEN_WIDTH/9} y={SCREEN_HEIGHT/10} width={SCREEN_WIDTH - SCREEN_WIDTH/9 - SCREEN_WIDTH/9} height={SCREEN_HEIGHT - SCREEN_HEIGHT/10 - (2 * SCREEN_HEIGHT/10)} fill="orange" strokeWidth="2" stroke="orange" strokeOpacity=".8" fillOpacity="0" />
                 <Rect x={0} y={0} width={SCREEN_WIDTH/9} height={SCREEN_HEIGHT} fill="orange" strokeWidth="0" stroke="orange" strokeOpacity=".6" fillOpacity=".2" />
@@ -72,6 +71,17 @@ export default class CameraExample extends React.Component {
                 <Rect x={SCREEN_WIDTH - SCREEN_WIDTH/9} y={SCREEN_HEIGHT/10} width={SCREEN_WIDTH} height={SCREEN_HEIGHT} fill="orange" strokeWidth="0" stroke="orange" strokeOpacity=".6" fillOpacity=".2" />
                 <Rect x={SCREEN_WIDTH/9} y={SCREEN_HEIGHT - SCREEN_HEIGHT/10 - (1 * SCREEN_HEIGHT/10)} width={SCREEN_WIDTH- 2*SCREEN_WIDTH/9} height={SCREEN_HEIGHT} fill="orange" strokeWidth="0" stroke="green" strokeOpacity=".6" fillOpacity=".2" />
               </Svg>
+              <TouchableOpacity style={{
+                  position: "absolute",
+                  top: '77%',
+                  right: '41%',
+                  alignItems: 'center',
+                }}
+                onPress={this.snapPhoto.bind(this)}>
+                <Image style={{width: 75, height: 75}} source={require('../assets/capture.png')}          
+                />
+              </TouchableOpacity>
+
             </View>
           </Camera>
         </View>
