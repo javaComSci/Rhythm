45fa927f79021cd2fa03cc9ca5f010eaca1e7ae7
dellamoresteven <dellamoresteven@gmail.com>
2019-04-05 12:54:40 -0400

    Camera Box(s)

--- Frontend/components/Camera.js
+++ Frontend/components/Camera.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import { Camera, Permissions } from 'expo';
+  // import { Header } from 'react-navigation';
+  import { connect } from 'react-redux';
+  import { Button, Header } from 'react-native-elements';
+  import { Col, Row, Grid } from "react-native-easy-grid";
+  import Icon from 'react-native-vector-icons/AntDesign'
+  import PinchZoomView from 'react-native-pinch-zoom-view';
+
+
+  import { Svg } from 'expo';
+  const { Circle, Rect, Path, Line, G, Defs, Use } = Svg;
+  const SCREEN_WIDTH = Dimensions.get('window').width
+  const SCREEN_HEIGHT = Dimensions.get('window').height
 
 export default class CameraExample extends React.Component {
   state = {
@@ -47,6 +59,13 @@ export default class CameraExample extends React.Component {
                   {' '}Flip{' '}
                 </Text>
               </TouchableOpacity>
+              <Svg height="100%"  width="100%">
+                <Rect x={SCREEN_WIDTH/9} y={SCREEN_HEIGHT/10} width={SCREEN_WIDTH - SCREEN_WIDTH/9 - SCREEN_WIDTH/9} height={SCREEN_HEIGHT - SCREEN_HEIGHT/10 - (2 * SCREEN_HEIGHT/10)} fill="orange" strokeWidth="2" stroke="orange" strokeOpacity=".8" fillOpacity="0" />
+                <Rect x={0} y={0} width={SCREEN_WIDTH/9} height={SCREEN_HEIGHT} fill="orange" strokeWidth="0" stroke="orange" strokeOpacity=".6" fillOpacity=".2" />
+                <Rect x={SCREEN_WIDTH/9} y={0} width={SCREEN_WIDTH} height={SCREEN_HEIGHT/10} fill="orange" strokeWidth="0" stroke="orange" strokeOpacity=".6" fillOpacity=".2" />
+                <Rect x={SCREEN_WIDTH - SCREEN_WIDTH/9} y={SCREEN_HEIGHT/10} width={SCREEN_WIDTH} height={SCREEN_HEIGHT} fill="orange" strokeWidth="0" stroke="orange" strokeOpacity=".6" fillOpacity=".2" />
+                <Rect x={SCREEN_WIDTH/9} y={SCREEN_HEIGHT - SCREEN_HEIGHT/10 - (1 * SCREEN_HEIGHT/10)} width={SCREEN_WIDTH- 2*SCREEN_WIDTH/9} height={SCREEN_HEIGHT} fill="orange" strokeWidth="0" stroke="green" strokeOpacity=".6" fillOpacity=".2" />
+              </Svg>
             </View>
           </Camera>
         </View>
