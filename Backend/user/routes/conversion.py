db6179d4de7efb14678fe014164b81f14ed704b6
javaComSci <iramanat@purdue.edu>
2019-04-15 13:00:37 -0400

    Conversion changes

--- Backend/user/routes/conversion.py
+++ Backend/user/routes/conversion.py
@@ -10,137 +10,138 @@ import sendToCloud
 # from .. import MySQL
 
 def conv(filepaths):
-        print("IN CONVERSION")
+    print("IN CONVERSION")
 	bigData = {}
 	bigData['clef'] = 1
 	bigData['notes'] = []
         
-        count = 0
-        sheet_id = ""
+    count = 0
+    sheet_id = ""
 	for file_path in filepaths:
-                if count == 0:
-                    indy = file_path.find('-')
-                    for i in range(indy-1, 0, -1):
-                        if file_path[i] == '/':
-                            break
+        if count == 0:
+        indy = file_path.find('-')
+        for i in range(indy-1, 0, -1):
+            if file_path[i] == '/':
+                break
 
-                        sheet_id = file_path[i] + sheet_id
-                count = 1
+	sheet_id = file_path[i] + sheet_id
+	count = 1
 
-                print("SHEET ID", sheet_id)
-                print("FILE PATH", file_path)
+    print("SHEET ID", sheet_id)
+    print("FILE PATH", file_path)
                 
-		SOL, shape, sl = partition.full_partition(file_path)
-                print("AFTER PARITIOTN")
-		ob_counter = 0
+	SOL, shape, sl = partition.full_partition(file_path)
+    print("AFTER PARITIOTN")
+	ob_counter = 0
 
-		#partition.print_objects(mask, SOL, staff_lines, "ExamplePredictions/predictions/")
-		#cv2.imwrite("ExamplePredictions/predictions/full_img.jpg", mask)
+	#partition.print_objects(mask, SOL, staff_lines, "ExamplePredictions/predictions/")
+	#cv2.imwrite("ExamplePredictions/predictions/full_img.jpg", mask)
 
-		gclef = False
-		cclef = False
-		fclef = False
+	gclef = False
+	cclef = False
+	fclef = False
 
-		for i in range(len(SOL)):
-			print("I", i)
-			n_arr = partition.SO_to_array(SOL[i])
+	for i in range(len(SOL)):
+		print("I", i)
+		n_arr = partition.SO_to_array(SOL[i])
 
-			#print(n_arr.shape)
+		#print(n_arr.shape)
 
-			flat_arr = n_arr.flatten().reshape((1,3500))
+		flat_arr = n_arr.flatten().reshape((1,3500))
 
 
-			ob_prediction = neuralNet.predict(flat_arr)
-			# im = im.reshape((70,50)) * 255
-			if ob_prediction == None or len(ob_prediction) == 0:
-				ob_prediction = "DEFAULT"
+		ob_prediction = neuralNet.predict(flat_arr)
+		print("THE PREDICTION RECIEVED IS")
+		# im = im.reshape((70,50)) * 255
+		if ob_prediction == None or len(ob_prediction) == 0:
+			ob_prediction = "DEFAULT"
 
-			#cv2.imwrite("ExamplePredictions/predictions/ob#{}_label:{}.jpg".format(ob_counter, ob_prediction[0]), im)
+		#cv2.imwrite("ExamplePredictions/predictions/ob#{}_label:{}.jpg".format(ob_counter, ob_prediction[0]), im)
 
-			data = {}
+		data = {}
 
-			clefCount = 0
+		clefCount = 0
 
-			#print("OB PREDICTION", ob_prediction[0])
+		#print("OB PREDICTION", ob_prediction[0])
 
-			if ob_prediction == 'GClef':
-				SOL[i].clef = 1
-				data['note'] = 0
-				data['pitch'] = 1
-				data['length'] = 0
-				gclef = True
-			elif ob_prediction == 'CClef':
-				SOL[i].clef = 2
-				data['note'] = 6
-				data['pitch'] = 1
-				data['length'] = 0
-				cclef = True
-			elif ob_prediction == 'FClef':
-				SOL[i].clef = 3
-				data['note'] = 7
-				data['pitch'] = 1
-				data['length'] = 0
-				fclef = True
-			elif ob_prediction == 'Sixteenth-Note':
-				SOL[i].duration = .125
-				data['note'] = 5
-				data['pitch'] = SOL[i].run
-				data['length'] = .125
-			elif ob_prediction == 'Eighth-Note':
-				SOL[i].duration = .5
-				data['note'] = 1
-				data['pitch'] = SOL[i].run
-				data['length'] = .5
-			elif ob_prediction == 'Quarter-Note':
-				SOL[i].duration = 1
-				data['note'] = 2
-				data['pitch'] = SOL[i].run
-				data['length'] = 1
-			elif ob_prediction == 'Half-Note':
-				SOL[i].durationn = 2
-				data['note'] = 3
-				data['pitch'] = SOL[i].run
-				data['length'] = 2
-			elif ob_prediction == 'Whole-Note':
-				SOL[i].duration = 4
-				data['note'] = 8
-				data['pitch'] = SOL[i].run
-				data['length'] = 4
-			elif ob_prediction == 'Eighth-Rest':
-				SOL[i].rest = .5
-				data['note'] = 9
-				data['pitch'] = SOL[i].run
-				data['length'] = .5
-			elif ob_prediction == 'Quarter-Rest':
-				SOL[i].rest = 1
-				data['note'] = 10
-				data['pitch'] = SOL[i].run
-				data['length'] = 1
-			elif ob_prediction == 'Whole-Half-Rest':
-				SOL[i].rest = 2
-				data['note'] = 11
-				data['pitch'] = SOL[i].run
-				data['length'] = 2
-			elif ob_prediction == 'Sharp':
-				SOL[i].accidental = 1
-				data['note'] = 13
-				data['pitch'] = SOL[i].run
-				data['length'] = 0
-			elif ob_prediction == 'Flat':
-				SOL[i].accidental = 2
-				data['note'] = 14
-				data['pitch'] = SOL[i].run
-				data['length'] = 0
+		if ob_prediction[0] == 'GClef':
+			SOL[i].clef = 1
+			data['note'] = 0
+			data['pitch'] = 1
+			data['length'] = 0
+			gclef = True
+		elif ob_prediction[0] == 'CClef':
+			SOL[i].clef = 2
+			data['note'] = 6
+			data['pitch'] = 1
+			data['length'] = 0
+			cclef = True
+		elif ob_prediction[0] == 'FClef':
+			SOL[i].clef = 3
+			data['note'] = 7
+			data['pitch'] = 1
+			data['length'] = 0
+			fclef = True
+		elif ob_prediction[0] == 'Sixteenth-Note':
+			SOL[i].duration = .125
+			data['note'] = 5
+			data['pitch'] = SOL[i].run
+			data['length'] = .125
+		elif ob_prediction[0] == 'Eighth-Note':
+			SOL[i].duration = .5
+			data['note'] = 1
+			data['pitch'] = SOL[i].run
+			data['length'] = .5
+		elif ob_prediction[0] == 'Quarter-Note':
+			SOL[i].duration = 1
+			data['note'] = 2
+			data['pitch'] = SOL[i].run
+			data['length'] = 1
+		elif ob_prediction[0] == 'Half-Note':
+			SOL[i].durationn = 2
+			data['note'] = 3
+			data['pitch'] = SOL[i].run
+			data['length'] = 2
+		elif ob_prediction[0] == 'Whole-Note':
+			SOL[i].duration = 4
+			data['note'] = 8
+			data['pitch'] = SOL[i].run
+			data['length'] = 4
+		elif ob_prediction[0] == 'Eighth-Rest':
+			SOL[i].rest = .5
+			data['note'] = 9
+			data['pitch'] = SOL[i].run
+			data['length'] = .5
+		elif ob_prediction[0] == 'Quarter-Rest':
+			SOL[i].rest = 1
+			data['note'] = 10
+			data['pitch'] = SOL[i].run
+			data['length'] = 1
+		elif ob_prediction[0] == 'Whole-Half-Rest':
+			SOL[i].rest = 2
+			data['note'] = 11
+			data['pitch'] = SOL[i].run
+			data['length'] = 2
+		elif ob_prediction[0] == 'Sharp':
+			SOL[i].accidental = 1
+			data['note'] = 13
+			data['pitch'] = SOL[i].run
+			data['length'] = 0
+		elif ob_prediction[0] == 'Flat':
+			SOL[i].accidental = 2
+			data['note'] = 14
+			data['pitch'] = SOL[i].run
+			data['length'] = 0
 
-			#print(SOL[i].duration, SOL[i].accidental)
+		#print(SOL[i].duration, SOL[i].accidental)
 
-			ob_counter += 1
+		ob_counter += 1
 
-			bigData['notes'].append(data)
+		bigData['notes'].append(data)
 
-			#print("NOTES", bigData["notes"])
+		#print("NOTES", bigData["notes"])
 
-			#print("DATA", data)
+		#print("DATA", data)
 
 		if gclef == True and fclef == True:
 			bigData['clef'] = 1
@@ -155,8 +156,6 @@ def conv(filepaths):
     print("before write")
 	jsonData = json.dumps(bigData)
 
-	#print("bigData")
-	#print(bigData)
 
 	with open('{}.txt'.format(sheet_id), 'w') as outfile:  
 		json.dump(jsonData, outfile)
